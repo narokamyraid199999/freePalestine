439f14a4ec46cd1e61d9f15134d6eac7a618901a
narokamyraid199999 <143643793+narokamyraid199999@users.noreply.github.com>
2024-05-15 19:42:18 -0700

    responsev 3

--- src/app/main/components/people/people.component.ts
+++ src/app/main/components/people/people.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/core/services/user.service';
 import { UserRes } from '../../core/interfaces/user-res';
+import { baseUrl } from 'src/app/shared/services/autht.service';
 
 function shuffle(array: any[]) {
   let currentIndex = array.length;
@@ -51,4 +52,5 @@ export class PeopleComponent implements OnInit {
 
   loading: boolean = false;
   users: UserRes[] = [];
+  url: string = baseUrl;
 }
