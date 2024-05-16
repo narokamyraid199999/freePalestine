954ffcebf6261413dbb09cafacc2f76d83f3a1eb
narokamyraid199999 <143643793+narokamyraid199999@users.noreply.github.com>
2024-05-16 10:59:00 -0700

    saved page changed

--- src/app/main/components/saved/saved.component.ts
+++ src/app/main/components/saved/saved.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { postData, PostElm } from '../../core/interfaces/post-elm';
+import { PostService } from 'src/app/shared/services/post.service';
+import { Message, MessageService } from 'primeng/api';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
+import { UserRes } from '../../core/interfaces/user-res';
 
 @Component({
   selector: 'app-saved',
   templateUrl: './saved.component.html',
-  styleUrls: ['./saved.component.css']
+  styleUrls: ['./saved.component.css'],
 })
-export class SavedComponent {
+export class SavedComponent implements OnInit {
+  constructor(
+    private _postService: PostService,
+    private _messageService: MessageService,
+    private _activateRouter: ActivatedRoute,
+    private _userService: UserService
+  ) {}
 
+  ngOnInit(): void {
+    this.getUserId();
+    this.getUserById();
+  }
+
+  getUserId() {
+    this.userId = parseInt(`${localStorage.getItem('token')}`);
+  }
+
+  getUserById() {
+    this.loading = true;
+    this._userService.getUserById(this.userId).subscribe({
+      next: (data) => {
+        this.user = data.data;
+        console.log('user data', this.user);
+        this.allPosts = this.user?.attributes.savedPosts.data;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.log('user error', error);
+        this.loading = false;
+      },
+    });
+  }
+
+  // getAllPosts(): void {
+  //   this.loading = true;
+  //   this._postService.getAllPosts().subscribe({
+  //     next: (data) => {
+  //       this.allPosts = data.data;
+  //       console.log(this.allPosts);
+  //       this.loading = false;
+  //     },
+  //     error: (error) => {
+  //       console.log('error getting all posts', error);
+  //       this.loading = false;
+  //     },
+  //   });
+  // }
+
+  refreshPage(event: any) {
+    this._messageService.add({
+      severity: 'info',
+      detail: 'post has been removed',
+    });
+
+    setTimeout(() => {
+      this.messages = [];
+    }, 7000);
+    this.getUserById();
+  }
+
+  allPosts: PostElm[] = [];
+  loading: boolean = false;
+  messages: Message[] | undefined;
+  userId: number = 0;
+  user: UserRes | undefined;
 }
